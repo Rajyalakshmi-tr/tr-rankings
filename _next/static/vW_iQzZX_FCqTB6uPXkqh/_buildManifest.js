6d6b05876a18990d00b6bfcf1c2242c215458a0a
TRKM-world-rankings <rajyalakshmi.puppala@mail.com>
2026-07-31 14:53:59 +0530

    Updates

---
+++ _next/static/vW_iQzZX_FCqTB6uPXkqh/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/tr-rankings//_next/:path+",
+        "destination": "/tr-rankings/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
