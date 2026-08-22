b7e540ce0365b4e72b55a45e236e1d2faa531780
TRKM-world-rankings <rajyalakshmi.puppala@mail.com>
2026-08-22 11:58:08 +0530

    Fix Open Graph image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/tr-rankings",
+  assetPrefix: "/tr-rankings/",
   images: {
     unoptimized: true,
   },
